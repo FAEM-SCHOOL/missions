66486d734ca29664a66806e898006275c0fff319
Zaurbek Tedeev <poker.noob@yandex.ru>
2020-07-19 00:29:36 +0300

    Add new command 'Uncomplete'

--- task/cmd/uncomplete.go
+++ task/cmd/uncomplete.go
@@ -18,13 +18,12 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"time"
 )
 
-// doCmd represents the do command
-var doCmd = &cobra.Command{
-	Use:   "do",
-	Short: "Marks a task complete ",
+// uncompleteCmd represents the uncomplete command
+var uncompleteCmd = &cobra.Command{
+	Use:   "uncomplete",
+	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -32,64 +31,40 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
 		tasks := ReadJsonFile("tasks.json")
+		for i, v := range tasks{
 
-		t := time.Now()
-		var date string
-		var task string
+			var uncomplete []Task
+			flag := false
 
-		if len(args) > 1{
-			date = args[1]
-			task = args[0]
-		}else{
-			date = t.Format("01-02-2006")
-			task = args[0]
-		}
-
-		if(!TaskExist(tasks, task, date) && !DateExist(tasks, date)) {
-			fmt.Println("Такой задачи и дня нет")
-
-		}else{
-			if !DateExist(tasks,date) {
-				fmt.Println("Такого дня нет")
-			}else {
-				if !TaskExist(tasks, task, date) {
-					fmt.Println("Такой задачи нет")
-				} else{
-					for i,  v := range tasks{
-						if i == date{
-							for e := 0; e < len(v); e++ {
-								if (v[e].Name == task) {
-									v[e].IsComplete = "!!Выполнено!!"
-								}
-							}
-						}
-					}
-
-
+			for e := 0; e < len(v); e++{
+				if v[e].IsComplete == "Выполняется"{
+					uncomplete = append(uncomplete, v[e])
+					flag = true
 				}
 			}
-		}
-		
 
-		WriteJsonFile("tasks.json", tasks)
+			if flag {
+				fmt.Println(i)
+				for e := 0; e < len(uncomplete); e++{
+					fmt.Println("  ", uncomplete[e].Name)
+				}
+			}
+
+		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(doCmd)
+	rootCmd.AddCommand(uncompleteCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// doCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	//doCmd.Flags().StringP("do", "d", "", "Marks the tasks completed")
+	// uncompleteCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// doCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
+	// uncompleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
