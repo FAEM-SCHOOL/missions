7e2df2750e2bfdc749f5a15000fef39d2b6080cb
Vorpex13 <solo_solo_2014@mai.ru>
2020-07-20 11:23:13 +0300

    Проверка пароля на сложность ver 1.0.

---
+++ m003/Password.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+)
+
+var command string
+var number bool
+var lower bool
+var upper bool
+var special bool
+
+//Проверка пароля на наличие необходимых символов.
+func chek(){
+	if len(command)<6{
+		fmt.Println("Длинв пароля слишком мала!!!")
+		return
+	}
+
+	for i:=0;i<len(command);i++{
+		numbers  :=  "0123456789"
+		for j:=0;j<len(numbers);j++{
+			if command[i]==numbers[j]{
+				number=true
+				break
+			}
+		}
+	}
+	if !number {
+		fmt.Println("НЕТ ЦИФР  В ПАРОЛЕ!!!")
+	}
+
+	for i:=0;i<len(command);i++{
+		lower_case  :=  "abcdefghijklmnopqrstuvwxyz"
+		for j:=0;j<len(lower_case);j++{
+			if command[i]==lower_case[j]{
+				lower=true
+				break
+		}}
+	}
+	if !lower {
+		fmt.Println("НЕТ ПРОПИСНЫХ БУКВ  В ПАРОЛЕ!!!")
+	}
+
+	for i:=0;i<len(command);i++{
+		upper_case  :=  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		for j:=0;j<len(upper_case);j++{
+			if command[i]==upper_case[j]{
+				upper=true
+				break
+			}
+		}
+	}
+	if !upper {
+		fmt.Println("НЕТ ЗАГЛАВНЫХ БУКВ В ПАРОЛЕ!!!")
+	}
+	for i:=0;i<len(command);i++{
+		special_characters  :=  "!@#$%^&*()-+"
+		for j:=0;j<len(special_characters);j++{
+			if command[i]==special_characters[j]{
+				special=true
+				break
+			}
+		}
+	}
+	if !special {
+		fmt.Println("НЕТ СПЕЦ СИМВОЛОВ В ПАРОЛЕ!!!")
+	}
+}
+func main() {
+	fmt.Print("Введите пароль для проверки :")
+	fmt.Scan(&command)
+	chek()
+	if special && lower && number && upper{
+		fmt.Println("Ваш пароль сильный!!!")
+	}
+}
