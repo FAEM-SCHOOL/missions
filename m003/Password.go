3c49d0cf985e0da44b12dfc0a3119ab47e1545b3
Vorpex13 <solo_solo_2014@mai.ru>
2020-07-24 09:41:26 +0300

    Изменение файла описания, разбивка на функции и форматирование.

--- m003/Password.go
+++ m003/Password.go
@@ -11,42 +11,47 @@ var upper bool
 var special bool
 
 //Проверка пароля на наличие необходимых символов.
-func chek(){
-	if len(command)<6{
+func chek_length() {
+	if len(command) < 6 {
 		fmt.Println("Длинв пароля слишком мала!!!")
 		return
 	}
-
-	for i:=0;i<len(command);i++{
-		numbers  :=  "0123456789"
-		for j:=0;j<len(numbers);j++{
-			if command[i]==numbers[j]{
-				number=true
+}
+func chek_number() {
+	for i := 0; i < len(command); i++ {
+		numbers := "0123456789"
+		for j := 0; j < len(numbers); j++ {
+			if command[i] == numbers[j] {
+				number = true
 				break
 			}
 		}
 	}
+
 	if !number {
 		fmt.Println("НЕТ ЦИФР  В ПАРОЛЕ!!!")
 	}
-
-	for i:=0;i<len(command);i++{
-		lower_case  :=  "abcdefghijklmnopqrstuvwxyz"
-		for j:=0;j<len(lower_case);j++{
-			if command[i]==lower_case[j]{
-				lower=true
+}
+func chek_lower() {
+	for i := 0; i < len(command); i++ {
+		lower_case := "abcdefghijklmnopqrstuvwxyz"
+		for j := 0; j < len(lower_case); j++ {
+			if command[i] == lower_case[j] {
+				lower = true
 				break
-		}}
+			}
+		}
 	}
 	if !lower {
 		fmt.Println("НЕТ ПРОПИСНЫХ БУКВ  В ПАРОЛЕ!!!")
 	}
-
-	for i:=0;i<len(command);i++{
-		upper_case  :=  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		for j:=0;j<len(upper_case);j++{
-			if command[i]==upper_case[j]{
-				upper=true
+}
+func chek_upper() {
+	for i := 0; i < len(command); i++ {
+		upper_case := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		for j := 0; j < len(upper_case); j++ {
+			if command[i] == upper_case[j] {
+				upper = true
 				break
 			}
 		}
@@ -54,11 +59,13 @@ func chek(){
 	if !upper {
 		fmt.Println("НЕТ ЗАГЛАВНЫХ БУКВ В ПАРОЛЕ!!!")
 	}
-	for i:=0;i<len(command);i++{
-		special_characters  :=  "!@#$%^&*()-+"
-		for j:=0;j<len(special_characters);j++{
-			if command[i]==special_characters[j]{
-				special=true
+}
+func chek_special() {
+	for i := 0; i < len(command); i++ {
+		special_characters := "!@#$%^&*()-+"
+		for j := 0; j < len(special_characters); j++ {
+			if command[i] == special_characters[j] {
+				special = true
 				break
 			}
 		}
@@ -67,11 +74,16 @@ func chek(){
 		fmt.Println("НЕТ СПЕЦ СИМВОЛОВ В ПАРОЛЕ!!!")
 	}
 }
+
 func main() {
 	fmt.Print("Введите пароль для проверки :")
 	fmt.Scan(&command)
-	chek()
-	if special && lower && number && upper{
+	chek_length()
+	chek_number()
+	chek_lower()
+	chek_upper()
+	chek_special()
+	if special && lower && number && upper {
 		fmt.Println("Ваш пароль сильный!!!")
 	}
 }
